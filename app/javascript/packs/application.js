35d9f5e1387d6cbb57fb23e2a9f3c84c25b00e9b
bruno della mattia <brunodellamattia@gmail.com>
2018-05-18 11:03:58 +0200

    new mister

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,3 +8,7 @@
 // layout file, like app/views/layouts/application.html.erb
 
 console.log('Hello World from Webpacker')
+import '../components/select2';
+
+import { loadDynamicBannerText } from '../components/banner';
+loadDynamicBannerText();
